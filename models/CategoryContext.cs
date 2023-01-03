ae699f855341f31536a14ecc0539271b973950fe
Hillel Buchsbaum <Hzbbaum@gmail.com>
2023-01-03 21:23:02 +0200

    added model for category items

---
+++ models/CategoryContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Shopping_cart_api.models
+{
+    public class CategoryContext : DbContext
+    {
+        public CategoryContext(DbContextOptions<CategoryContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CategoryItem> CategoryItems { get; set; } = null!;
+    }
+}
---
+++ models/CategoryItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping_cart_api.models
+{
+    public class CategoryItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
