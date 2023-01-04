aa50290dc6214845a1752e4f24eb299c9507cb93
Hillel Buchsbaum <Hzbbaum@gmail.com>
2023-01-04 10:41:07 +0200

    Added a categoty items controller

---
+++ Controllers/CategoryItemsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping_cart_api.models;
+
+namespace Shopping_cart_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryItemsController : ControllerBase
+    {
+        private readonly CategoryContext _context;
+
+        public CategoryItemsController(CategoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CategoryItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryItem>>> GetCategoryItems()
+        {
+            if (_context.CategoryItems == null)
+            {
+                return NotFound();
+            }
+            return await _context.CategoryItems.ToListAsync();
+        }
+
+        // GET: api/CategoryItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryItem>> GetCategoryItem(long id)
+        {
+            if (_context.CategoryItems == null)
+            {
+                return NotFound();
+            }
+            var categoryItem = await _context.CategoryItems.FindAsync(id);
+
+            if (categoryItem == null)
+            {
+                return NotFound();
+            }
+
+            return categoryItem;
+        }
+
+
+        // POST: api/CategoryItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CategoryItem>> PostCategoryItem(CategoryItem categoryItem)
+        {
+            if (_context.CategoryItems == null)
+            {
+                return Problem("Entity set 'CategoryContext.CategoryItems'  is null.");
+            }
+            _context.CategoryItems.Add(categoryItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCategoryItem), new { id = categoryItem.Id }, categoryItem);
+        }
+
+        // DELETE: api/CategoryItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoryItem(long id)
+        {
+            if (_context.CategoryItems == null)
+            {
+                return NotFound();
+            }
+            var categoryItem = await _context.CategoryItems.FindAsync(id);
+            if (categoryItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.CategoryItems.Remove(categoryItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoryItemExists(long id)
+        {
+            return (_context.CategoryItems?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
